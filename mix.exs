6030a49096ba7c7db5aec64c6d460f6ac60ea5d2
Danny Cheng-Hsuan Han <chenghsuan.han@gmail.com>
2021-06-24 21:46:16 +0000

    Add dep "jsx"

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.8.0"}
+      {:httpoison, "~> 1.8.0"},
+      {:jsx, "~> 3.1.0"}
     ]
   end
 end
